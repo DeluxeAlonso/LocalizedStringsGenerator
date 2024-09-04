48e9a9040516f402681fd7aa6d50596fcd7b3ea1
DeluxeAlonso <alonso.alvarez@pucp.pe>
2024-09-04 00:31:11 -0500

    Updates Package config

--- Sources/LocalizedStringsGenerator/LocalizedStringsGenerator.swift
+++ Sources/LocalizedStringsGenerator/LocalizedStringsGenerator.swift
@@ -1,6 +1,7 @@
-@main
-public struct LocalizedStringsGenerator {
-    static func main() {
-        print("Hello world")
-    }
-}
+//@main
+//public struct LocalizedStringsGenerator {
+//    static func main() {
+//        print("Hello world")
+//    }
+//}
+print("Hello world")
