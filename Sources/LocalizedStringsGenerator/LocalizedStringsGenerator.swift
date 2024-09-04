c667ef46d438432c401d274ebcb540be668ac64d
DeluxeAlonso <alonso.alvarez@pucp.pe>
2024-09-04 00:31:26 -0500

    Updates Example project

--- Sources/LocalizedStringsGenerator/LocalizedStringsGenerator.swift
+++ Sources/LocalizedStringsGenerator/LocalizedStringsGenerator.swift
@@ -1,7 +1,6 @@
-//@main
-//public struct LocalizedStringsGenerator {
-//    static func main() {
-//        print("Hello world")
-//    }
-//}
-print("Hello world")
+@main
+public struct LocalizedStringsGenerator {
+    static func main() {
+        print("Hello world")
+    }
+}
