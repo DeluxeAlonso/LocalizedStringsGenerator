cbd69b185d0c024b8e90679d5cc8b0b5d455dbea
DeluxeAlonso <alonso.alvarez@pucp.pe>
2024-09-08 21:44:46 -0500

    Handles string file name argument

--- Example/Scripts/LocalizedStringsGenerator.swift
+++ Example/Scripts/LocalizedStringsGenerator.swift
@@ -4,18 +4,20 @@ import Foundation
 public struct LocalizedStringsGenerator {
     static func main() {
         print(CommandLine.arguments[1])
+        print(CommandLine.arguments[2])
         let filePath = CommandLine.arguments[1]
-        createLocalizedStringsFile(filePath)
+        let stringsFileName = CommandLine.arguments[2]
+        createLocalizedStringsFile(filePath, stringsFileName)
     }
 
-    private static func createLocalizedStringsFile(_ filePath: String) {
+    private static func createLocalizedStringsFile(_ filePath: String, _ stringsFileName: String) {
         let fileContentString =
         """
         \(imports)
 
         \(localizableProtocol)
 
-        \(stringExtension)
+        \(stringExtension(stringsFileName))
         """
 
         do {
@@ -27,7 +29,7 @@ public struct LocalizedStringsGenerator {
         
     }
 
-    // MARK: - Constants
+    // MARK: - Utils
 
     static let imports =
     """
@@ -52,14 +54,15 @@ public struct LocalizedStringsGenerator {
     }
     """
 
-    static let stringExtension =
-    """
-    private extension String {
-        func localized(bundle: Bundle = .main,
-                       tableName: String = "Localizable",
-                       comment: String = "") -> String {
-            NSLocalizedString(self, tableName: tableName, value: self, comment: comment)
+    static func stringExtension(_ stringsFileName: String) -> String {
+        """
+        private extension String {
+            func localized(bundle: Bundle = .main,
+                           tableName: String = "\(stringsFileName)",
+                           comment: String = "") -> String {
+                NSLocalizedString(self, tableName: tableName, value: self, comment: comment)
+            }
         }
+        """
     }
-    """
 }
